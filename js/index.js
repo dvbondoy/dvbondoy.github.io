c36311cccc3d2a73727beab1dbcf65a7a2402113
dvbondoy <dvbondoy@yahoo.com>
2023-03-16 11:48:52 +0800

    testing

---
+++ js/index.js
@@ -0,0 +1,27 @@
+function setVal(){
+    const d = new Date();
+    var str = d.toISOString().slice(0, -14)
+    var url = 'https://sourceforge.net/projects/flickos/files/stats/json?start_date=2023-03-08&end_date='+str;
+    
+    const stat = fetch(url)
+        .then((response) => response.json())
+        .then((json) => {
+            return json;
+        });
+    
+    const status = () => {
+        stat.then((a) => {
+            console.log(a);
+            document.getElementById("total").innerHTML = a.total;
+            console.log(a.total);
+        });
+    }
+    
+    status();
+}
+
+window.onload = function() {
+    setVal();
+}
+
+
